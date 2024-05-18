e1cb0eea2be941e0049f13e05374b209f9d56a3c
arveymenon <arveymenon@gmail.com>
2024-05-18 13:46:30 +0530

    update

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,34 @@
-import typescript from '@rollup/plugin-typescript';
-import css from 'rollup-plugin-css-only';
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import dts from "rollup-plugin-dts";
 
 const packageJson = require("./package.json");
 
-export default {
-    input: 'src/main.ts',
+export default [
+  {
+    input: "src/main.ts",
     output: [
-        {
-            file: packageJson.main,
-            format: "cjs",
-            sourcemap: true,
-        },
-        {
-            file: packageJson.module,
-            format: "esm",
-            sourcemap: true,
-        },
+      {
+        file: packageJson.main,
+        format: "cjs",
+        sourcemap: true,
+      },
+      {
+        file: packageJson.module,
+        format: "esm",
+        sourcemap: true,
+      },
     ],
     plugins: [
-        typescript(),
-        css({output: 'bundle.css'})
-    ]
-  };
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: "./tsconfig.json" }),
+    ],
+  },
+  {
+    input: "dist/esm/types/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    plugins: [dts()],
+  },
+];
